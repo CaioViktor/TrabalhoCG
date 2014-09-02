c856dfb892459882a94a647b143b8de8551d9d1f
Caio Viktor Avila <caioviktor@hotmail.com>
2014-09-02 18:47:40 -0300

    erro Matrix

--- src/testMatrix.cpp
+++ src/testMatrix.cpp
@@ -12,15 +12,25 @@ int main()
 		{9,10,11,12},
 		{13,14,15,16}
 	};
+    cout << "1:\n";
 	Matrix recebeVetor2d(m);
-
+    cout << "2:\n";
     identidade = Matrix::getIdentity();
-
+    cout << "3:\n";
     padrao.printMatrix();
-    identidade.printMatrix();
+    cout << "4:\n";
+    identidade.printMatrix();//falha segmentação
+    cout << "5:\n";
     recebeVetor2d.printMatrix();
+    cout << "6:\n";
     recebeVetor2d.getTransposed().printMatrix();
+    cout << "7:\n";
     recebeVetor2d.setPosition(1,0,9);
+    cout << "8:\n";
     recebeVetor2d.printMatrix();
+
+    cout << "nova matrix:\n";
+    Matrix *matrix = new Matrix(m);
+    cout << matrix->getPosition(0,0) << endl;
 
 }

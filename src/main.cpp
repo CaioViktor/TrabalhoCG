cd204e0213d5935b0bcdfcf60e173f0b335144b2
CaioViktor <caioviktor@hotmail.com>
2014-09-08 16:28:00 -0300

    [#78291230] Aplicados elementos estáticos da UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "../lib/main.h"
 GLfloat eyex,eyey,eyez,centrox, centroy, centroz;
 int sizeX,sizeY;
+int mainWindow;
+GLUI *glui;
 void draw(void) {
 
     // Black background
@@ -166,40 +168,141 @@ void input(unsigned char tecla, int x, int y)
     cout << "teclou\n";
     glutPostRedisplay();
 }
-void func(){
-    cout << "click!\n";
-}
-//Main program
 
+//callbacks GLUI interface
+void render(){
+    glutSetWindow(mainWindow);
+    glutPostRedisplay();
+}
+
+//Seleção de objetos
+GLUI_StaticText *textName, *textPosition, *textFace, *textVertex;
+int objSelected;
+string objectName = "NULL",objectPosition = "X:0;Y:0;Z:0", objectFaces = "0",objectVertex = "0";
+void selectObject(){
+    objectName = "oi";
+    //TODO: tratamento de informações
+    textName->set_text(objectName.c_str());
+    textPosition->set_text(objectPosition.c_str());
+    textFace->set_text(objectFaces.c_str());
+    textVertex->set_text(objectVertex.c_str());
+}
+
+//Seleção de tranformação
+int transformationSelected;
+void selectTransformation(){
+    switch(transformationSelected){
+        //Translação
+        case 0:
+            break;
+        //Rotação
+        case 1:
+            break;
+        //Escalamento
+        case 2:
+            break;
+    }
+    cout << transformationSelected << endl;
+}
+
+//Confirmar Transformação
+void confirmTransformation(){
+    //TODO:resto.
+    cout << "transformações confirmadas\n";
+}
+
+//Aplicar Transformação
+void applyTransformation(){
+    //TODO:resto.
+    cout << "transformações aplicadas\n";   
+}
+
+//Anular Transformação
+void cancelTransformation(){
+    //TODO:resto.
+    cout << "transformações canceladas\n";      
+}
+// int check;
+// void func1(){
+//     cout << check << endl;
+// }
+
+//Main program
 int main(int argc, char **argv) {
     sizeX = 1000;
     sizeY = 1000;
-    int mainWindow;
+    
     glutInit(&argc, argv);
-
-    /*Setting up  The Display
+    
+    /*Configura a tela
     /    -RGB color model + Alpha Channel = GLUT_RGBA
     */
     glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
 
-    //Configure Window Postion
+    //Configura a posição da janela
     glutInitWindowPosition(0, 0);
 
-    //Configure Window Size
+    //Configura o tamanho da janela
     glutInitWindowSize(sizeX,sizeY);
 
-    //Create Window
+    //Criação da janela
     mainWindow = glutCreateWindow("CG");
     glutKeyboardFunc(input);
     init();
     glutReshapeFunc(reshape);
-    //Call to the drawing function
+    //Chamada da função de desenhar
     glutDisplayFunc(draw);
 
     //GLUI
-    GLUI *glui = GLUI_Master.create_glui ("CG_GUI",0,sizeX+1,0);
-    glui->add_button("Click",0,(GLUI_Update_CB) func); 
+    glui = GLUI_Master.create_glui_subwindow( mainWindow,GLUI_SUBWINDOW_RIGHT );
     glui->set_main_gfx_window(mainWindow);
+    
+    //painel de objetos em cena
+    GLUI_Panel *objPanel = glui->add_panel( "Objetos em cena" );
+    GLUI_RadioGroup *group1 = glui->add_radiogroup_to_panel(objPanel,&objSelected,3,(GLUI_Update_CB)selectObject);
+    //TODO:Criação dinâmica
+    glui->add_radiobutton_to_group( group1, "Sphere" );
+    glui->add_radiobutton_to_group( group1, "Torus" );
+
+    //dados do objeto selecionado
+    glui->add_statictext( "Nome:" );
+    textName = glui->add_statictext( objectName.c_str() );
+    glui->add_statictext( "Posicao:" );
+    textPosition = glui->add_statictext( objectPosition.c_str() );
+    glui->add_statictext( "Faces:" );
+    textFace = glui->add_statictext( objectFaces.c_str() );
+    glui->add_statictext( "Vertices:" );
+    textVertex = glui->add_statictext( objectVertex.c_str() );
+    glui->add_separator();
+    //dados do objeto selecionado
+    glui->add_column(true); 
+    
+
+    //Transformações
+    GLUI_Panel *transformationsPanel = glui->add_panel( "Transformações" );
+    GLUI_RadioGroup *group2 = glui->add_radiogroup_to_panel(transformationsPanel,&transformationSelected,3);
+    //TODO:Criação dinâmica
+    glui->add_radiobutton_to_group( group2, "Translacao" );
+    glui->add_radiobutton_to_group( group2, "Rotacao" );
+    glui->add_radiobutton_to_group( group2, "Escala" );
+    glui->add_button("Adicionar",0,(GLUI_Update_CB)selectTransformation); 
+    glui->add_separator();
+    glui->add_button("Renderizar",0,(GLUI_Update_CB) render); 
+
+    glui->add_column(true); 
+
+    //parâmetros
+    glui->add_statictext( "Parametros" );
+    
+
+    //TODO: Entradas de parâmetros para cada transformação
+
+
+    glui->add_button("Confirmar",0,(GLUI_Update_CB) confirmTransformation); 
+    glui->add_button("Aplicar",0,(GLUI_Update_CB) applyTransformation); 
+    glui->add_button("Anular",0,(GLUI_Update_CB) cancelTransformation); 
+    //glui->add_checkbox("click me",&check,187,(GLUI_Update_CB)func1);
+    
     // Loop require by OpenGL
     glutMainLoop();
     return 0;

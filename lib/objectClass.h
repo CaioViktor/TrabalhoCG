21ba923a269b7edde5600e5d280d785d0c7fa8db
cristmelo <cristianomelo_88@hotmail.com>
2014-09-14 20:43:19 -0300

    [#77824972] edição da classe ObjectClass
    edição da classe ObjectClass

--- lib/objectClass.h
+++ lib/objectClass.h
@@ -1,19 +1,44 @@
 /* object.h - Cristiano Melo e Lucas Falcão
 *  Implemanta a estrutra de objeto
 */
+
 class ObjectClass{
-	std::string name;
-	ListVertex *list ;
-	Vertex *Centroid;
+    //Nome do objeto
+	std::string name; 
+	//Lista de Vertice
+	ListVertex *list_vertex ; 
+	//Numero de Vertices
+	int number_vertex;
+	//Lista de Face
+	ListFace *list_face; 
+	//Numero de Faces
+	int number_face;
+	//Cores:
+    float R;
+    float G;
+    float B;
+    
+	//Vertex *Centroid;
 	
 public:
-	//Builders	
+	//Builder	
 	ObjectClass();
-	ObjectClass(std::string name, ListVertex *list );
-	ObjectClass(std::string name, ListVertex list, Vertex Centroid );
 	
+	
+	void drawObject( unsigned int mode );
+	void applyTransformation( Matrix *transformationMatrix );
 	void setName(std::string Name );
+	void setListVertex( ListVertex *list_vertex );
+	void setListFace( ListFace *list_face );
+	void setColorR( float R );
+	void setColorG( float G );
+	void setColorB( float B );
 	std::string getName();
-	void setListVertex( ListVertex *list );
 	ListVertex* getListVertex();
+	ListFace* getListFace();
+	float getCollorR();
+	float getCollorG();
+	float getCollorB();
+	int getNumberVertex();
+	int getNumberFace();
 };
--- src/objectClass.cpp
+++ src/objectClass.cpp
@@ -2,35 +2,110 @@
 
 //Construtor da classe Object. 
 ObjectClass::ObjectClass(){
-	
-	this->name = "";
-		
+	//Nome do objeto
+    this->name = "";
 	//Lista de Vertices
-	this->list = NULL;
-	
+	this->list_vertex = NULL;
+	//Numero de Vertices
+	this->number_vertex = 0;
 	//Lista de Faces
-
+    this->list_face = NULL;
+    //Numero de Faces
+    this->number_face = 0;
+    
+    //Cores
+    this->R = 0;
+    this->G = 0.7;
+    this->B = 0;
+    
 	//Centroide	
 	
 }
 
-ObjectClass::ObjectClass( std::string name, ListVertex *list ){
-	this->name = name;
-	this->list = list;	
+//Método para desenhar objeto
+void ObjectClass::drawObject( unsigned int modeExibitionValue ){
+    glColor3f(this->R,this->G,this->B);   
+    if(modeExibitionValue == GL_POLYGON)
+        this->list_face->drawSolid();
+    else
+        this->list_face->drawWired();
 }
 
+//Método para aplicar transformação no objeto
+void ObjectClass::applyTransformation( Matrix *transformationMatrix ){ 
+     this->list_vertex->transformation( transformationMatrix );
+}
+     
+
+//SETs
+
+//Configura o nome do objeto
 void ObjectClass::setName( std::string Name ){
 	this->name = Name;
 }
 
+//Configura a lista de vértice
+void ObjectClass::setListVertex( ListVertex *list_vertex ){
+	this->list_vertex = list_vertex;
+}
+
+//Configura a lista de faces
+void ObjectClass::setListFace( ListFace *list_face ){
+     this->list_face = list_face;
+}
+
+//Configura a cor R do objeto
+void ObjectClass::setColorR( float R ){
+     this->R = R;
+}
+
+//Configura a cor G do objeto
+void ObjectClass::setColorG( float G ){
+     this->G = G;
+}
+
+//Configura a cor B do objeto
+void ObjectClass::setColorB( float B ){
+     this->B = B;
+}
+
+
+//GETs
+
+//Acessa o nome do objeto
 std::string ObjectClass::getName(){
 	return this->name;
 }
 
-void ObjectClass::setListVertex( ListVertex *list ){
-	this->list = list;
-}
-	
+//Acessa a lista de vértices do objeto	
 ListVertex* ObjectClass::getListVertex(){
-	return this->list;
+	return this->list_vertex;
+}
+
+//Acessa a lista de faces do objeto
+ListFace* ObjectClass::getListFace(){
+    return this->list_face;
+}
+
+//Acessa a cor R do objeto
+float ObjectClass::getCollorR(){
+      return this->R;
+}
+
+//Acessa a cor G do objeto
+float ObjectClass::getCollorG(){
+      return this->G;
+}
+
+//Acessa a cor B do objeto
+float ObjectClass::getCollorB(){
+      return this->B;
+}
+      
+int ObjectClass::getNumberVertex(){
+    return this->number_vertex;
+}
+
+int ObjectClass::getNumberFace(){
+    return this->number_face;
 }

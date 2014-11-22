2c016504e7ec445e4055ee78350d0c48064d410a
Matheus Mayron <matheusmayron@gmail.com>
2014-11-22 18:34:57 -0300

    get e sets para Light position

--- lib/illumination.h
+++ lib/illumination.h
@@ -11,10 +11,15 @@ public:
 	Illumination(Vector* light_position,Vector* light_ambiente, Vector* light_intensity);
 
 	void setLightPosition(Vector* V);
+	void setLightPosition(double x, double y, double z);
 	Vector* getLightPosition();
+	double getLightPosition_X();
+	double getLightPosition_Y();
+	double getLightPosition_Z();
 
 	void setLightAmbient(Vector* v);
 	Vector* getLightAmbient();
+
 
 	void setLightIntesity(Vector* v);
 	Vector* getLightIntesity();
--- src/illumination.cpp
+++ src/illumination.cpp
@@ -19,14 +19,35 @@ Illumination::Illumination(Vector* light_position, Vector* light_ambiente, Vecto
 void Illumination::setLightPosition(Vector* v){
 	this->light_position = v;
 }
+
+void Illumination::setLightPosition(double x, double y, double z){
+	light_position->setValue(0,x);
+	light_position->setValue(1,y);
+	light_position->setValue(2,z);
+}
+
 Vector* Illumination::getLightPosition(){
 	return this->light_position;
 }
 
+double Illumination::getLightPosition_X(){
+	return light_position->getValue(0);
+}
+
+double Illumination::getLightPosition_Y(){
+	return light_position->getValue(1);
+}
+
+double Illumination::getLightPosition_Z(){
+	return light_position->getValue(2);
+}
 
 void Illumination::setLightAmbient(Vector* v){
 	this->light_ambiente = v;
 }
+
+
+
 Vector* Illumination::getLightAmbient(){
 	return this->light_ambiente;
 }
